8795fbb08e0b80cb6b15fd05d1a73c42a40efd7e
Parth Garg <parth.garg.sr@gmail.com>
2016-12-22 22:50:40 +0200

    project --fake img

--- JS/functions.js
+++ JS/functions.js
@@ -1,3 +1,28 @@
-$(document).ready(
-    
-);
+$(document).ready(function () {
+    photoBelt();
+    picLoad();
+});
+
+function photoBelt() {
+    $('.thumb-unit').click(function () {
+        $('.work-belt').css('left', '-100%');
+        $('.work-container').show();
+    });
+    $('.work-return').click(function() {
+        $('.work-belt').css('left', '0%');
+        $('.work-container').hide(800);
+    });
+};
+
+function picLoad() {
+    $.ajaxSetup({ cache: true});
+    $('.thumb-unit').click(function () {
+        var $this = $(this),
+            newTitle = $(this).find('strong').text(),
+            newfolder = $this.data('folder'),
+            spinner = '<div class="loader">Loading...</div>',
+            newHTML = '/Images/HTML-PICG/'+newfolder+'.html';
+        $('.pic-load').html(spinner).load(newHTML);
+        $('.project-title').text(newTitle);
+    });
+};
